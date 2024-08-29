6b0c44e0ab016e84436f1f1cdd3e74966ba8bfad
adityasharma0131 <adityasharma0431@gmail.com>
2024-08-29 14:47:08 +0530

    header img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import logo from "../assets/headerlogo.png";
 import {
   RiArrowRightUpLine,
   RiCloseLine,
@@ -23,7 +24,7 @@ const ResponsiveNavbar = () => {
   return (
     <header className="header" id="header">
       <nav className="nav container">
-        <img className="nav__logo" src="/src/assets/heroimg1.png" alt="Logo" />
+        <img className="nav__logo" src={logo} alt="Logo" />
 
         <div
           className={`nav__menu ${menuVisible ? "show-menu" : ""}`}
